e78711af88e68d999dba45e75ba7869bcfdcda1c
Muhammad Arief Ravendra <ravendraarief@gmail.com>
2025-07-16 00:17:03 +1000

    add some feature for Route Create and Update Users

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -3,13 +3,18 @@ import {
   getUsers,
   register,
   login,
+  createUser,
+  updateUser
 } from '../controllers/user.controller';
+import { isSuperAdmin } from '../middlewares/superadmin.middleware';
 import { protect } from '../middlewares/auth.middleware';
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
-router.get('/', protect, getUsers);
+router.get('/', protect, getUsers,);
+router.post('/', protect, isSuperAdmin, createUser);
+router.put('/:id', protect, isSuperAdmin, updateUser);
 
 export default router;
